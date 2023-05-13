4342fe7595ec42993f37461ddbbba3064e49cdae
larsvers <lars@canva.com>
2023-05-13 21:14:41 +0100

    Set up settings viewing pane

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<div class="border border-gray-200 shadow-2xl">Hello</div>
+<script lang="ts">
+	import Settings from '$lib/components/Settings.svelte';
+</script>
+
+<div class="m-auto mt-24 w-5/6 border border-gray-100 shadow-xl md:w-1/2">
+	<Settings />
+</div>
