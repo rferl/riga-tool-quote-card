16d644a11a70894503162f957cfef376392ca8be
larsvers <lars@canva.com>
2023-05-21 10:12:28 +0100

    Built out basic editor mark up
      This is temporary as it is going to be replaced with
      the Editor's markup in the future

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,59 @@
 <script lang="ts">
 	import Settings from '$lib/components/Settings.svelte';
+	import { onMount } from 'svelte';
+
+	// This is a temporary editor markup. Ultimately we want to have the
+	// same markup as on the Editor's /editor page. For that we need to
+	// get a library set up to use in the Editor and the base tool repo.
+	let w: number;
+	let h: number;
+
+	let w_editor: number;
+	let h_editor: number;
+
+	function adaptSize() {
+		w_editor = w;
+		h_editor = h;
+	}
+	function resize() {
+		// Ensure that the layout is updated before calling `adaptSize`
+		requestAnimationFrame(adaptSize);
+	}
+
+	onMount(() => {
+		resize();
+		window.addEventListener('resize', resize);
+		return () => {
+			window.removeEventListener('resize', resize);
+		};
+	});
 </script>
 
-<div class="m-auto mt-24 w-5/6 border border-gray-100 shadow-xl md:w-1/2">
-	<Settings />
+<div class="m-8">
+	<div class="mb-8">
+		<input
+			type="number"
+			name="w-ctrl"
+			id="w-ctrl"
+			bind:value={w_editor}
+			class="max-w-[100px] rounded p-2 text-xs"
+		/>
+		<input
+			type="number"
+			name="h-ctrl"
+			id="h-ctrl"
+			bind:value={h_editor}
+			class="max-w-[100px] rounded p-2 text-xs"
+		/>
+	</div>
+	<div class="flex flex-col md:flex-row">
+		<div class="w-5/6 md:mr-4 md:w-3/5" bind:clientWidth={w} bind:clientHeight={h}>
+			<div class="border border-gray-200" style="width: {w_editor}px; height: {h_editor}px">
+				Tool code here
+			</div>
+		</div>
+		<div class="w-5/6 border border-gray-100 shadow-xl md:w-2/5">
+			<Settings />
+		</div>
+	</div>
 </div>
