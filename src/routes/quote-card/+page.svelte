b0fc3ca9dceecd898b1cc90ce2027c4e1ab3d846
larsvers <lars@canva.com>
2023-05-21 18:04:49 +0100

    Fetch settings from within tool
      This is fetching the settings from a /settings route
      just for testing purposes. Ultimately, this will come
      from an API.

--- src/routes/quote-card/+page.svelte
+++ src/routes/quote-card/+page.svelte
@@ -1,1 +1,27 @@
-<div>Hello from the quote-card tool</div>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	// Fetch tool settings.
+	let settings = {};
+
+	async function getSettings() {
+		const response = await fetch('/settings/quote-card.json');
+		if (!response.ok) {
+			console.error('Error fetching quote-card.json:', response.status, response.statusText);
+			return {};
+		}
+		return await response.json();
+	}
+
+	onMount(() => {
+		(async () => {
+			settings = await getSettings();
+			console.log(settings);
+		})();
+	});
+</script>
+
+<div>Hello from the quote-card tool with the settings:</div>
+{#each Object.entries(settings) as setting}
+	<div>{setting[0]}: {setting[1]}</div>
+{/each}
