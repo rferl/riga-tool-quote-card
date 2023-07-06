eed0e4522ebc9c0ddf10739682b8cd8929cea0c0
larsvers <lars@canva.com>
2023-07-06 15:20:22 +0100

    Fix SSR issue with Settings helper func

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -2,7 +2,8 @@
 
 <script lang="ts">
 	// Helper funcs
-	function decodeHtml(html) {
+	function decodeHtml(html: string) {
+		if (typeof document === 'undefined') return html;
 		var txt = document.createElement('textarea');
 		txt.innerHTML = html;
 		return txt.value;
