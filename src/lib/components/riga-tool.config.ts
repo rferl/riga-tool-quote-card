277eccc13621a1569e9ff896359f3afd197e2453
larsvers <lars@canva.com>
2023-09-07 11:19:19 +0100

    Build app

--- src/lib/components/riga-tool.config.ts
+++ src/lib/components/riga-tool.config.ts
@@ -4,7 +4,7 @@ export default {
   "category": "Editorial",
   "package_name": "riga-editor-tool-quote-card",
   "image": "https://i.ibb.co/JkdZrXj/Blueprint-0.jpg",
-  "url": "http://localhost:5174/index",
+  "url": "https://nice-bush-008d43c03.3.azurestaticapps.net/",
   "settings": {
     "quote_text": "Hello",
     "quote_symbol": "&#10078;",
