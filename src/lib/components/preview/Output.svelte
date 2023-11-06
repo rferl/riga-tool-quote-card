95402edb5105f3fb57acb623f9988d96a3be58e0
larsvers <lars@canva.com>
2023-11-06 23:55:01 +0000

    Refine output preview width

--- src/lib/components/preview/Output.svelte
+++ src/lib/components/preview/Output.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { output } from '$lib/stores';
+	import { previewWidth } from '$lib/stores';
 </script>
 
-<div class="bg-gray-25 m-4 p-4 pb-2 shadow-lg outline outline-gray-100">
+<div
+	class="bg-gray-25 m-4 min-w-[300px] p-4 pb-2 shadow-lg outline outline-gray-100"
+	style="width:{$previewWidth}px"
+>
 	<div class="font-bold text-gray-500">{$output.title}</div>
 	<div class="my-1 text-sm text-gray-400">{$output.description}</div>
 	<div class="my-2 rounded border border-gray-100 bg-white p-3 text-xs text-gray-500">
